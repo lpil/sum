1a9947e71783820f8e94a6881dbeccdb558a03af
Louis Pilfold <louis@lpil.uk>
2017-11-18 18:39:29 +0000

    Atom checking

--- lib/sum.ex
+++ lib/sum.ex
@@ -1,15 +1,54 @@
 defmodule Sum do
-  defmacro defsum({name, _, arg}) when arg in [nil, []] do
+  defmacro defsum({type_name, _, arg}) when arg in [nil, []] do
     quote bind_quoted: binding() do
       type_clauses =
         Module.get_attribute(__MODULE__, :type)
-        |> Sum.get_clauses!(name)
+        |> Sum.get_clauses!(type_name)
 
-      defmacro unquote(:"#{name}_case")(item, do: clauses) do
-        # TODO: Check all cases are accounted for
-        {:case, [], [item, [do: clauses]]}
+      defmacro unquote(:"#{type_name}_case")(subject, do: case_clauses) do
+        Sum.build_case!(
+          unquote(type_name),
+          subject,
+          unquote(Macro.escape(type_clauses)),
+          case_clauses
+        )
       end
     end
+  end
+
+  @doc false
+  def build_case!(name, item, types, case_clauses) do
+    patterns =
+      for {:->, _, [[pattern], _]} <- case_clauses do
+        pattern
+      end
+
+    unmatched =
+      Enum.filter(types, fn type ->
+        not Enum.any?(patterns, &matches?(&1, type))
+      end)
+
+    if unmatched == [] do
+      {:case, [], [item, [do: case_clauses]]}
+    else
+      # TODO: Nice error
+      throw({:incomplete, name, unmatched})
+    end
+  end
+
+  @doc false
+  def matches?(pattern, {:atom, _, _}) when is_atom(pattern) do
+    true
+  end
+
+  def matches?(atom_literal, type) when is_atom(atom_literal) do
+    atom_literal == type
+  end
+
+  # TODO: handle when
+  # {:when, [line: 81], [1, {:is_list, [line: 81], [{:thing, [line: 81], nil}]}]}
+  def matches?(_pattern, _type) do
+    true
   end
 
   @doc false
@@ -44,7 +83,7 @@ defmodule Thingy do
 
   def run(thing) do
     t_case thing do
-      1 ->
+      1 when is_list(thing) ->
         "one"
 
       _ ->
--- test/sum_test.exs
+++ test/sum_test.exs
@@ -2,6 +2,8 @@ defmodule SumTest do
   use ExUnit.Case
   doctest Sum
 
+  defstruct [:field]
+
   describe "parse_types/1" do
     test "empty" do
       types = []
@@ -48,6 +50,59 @@ defmodule SumTest do
       assert Sum.parse_types(types) == %{
                t: [:a, :b, :c, :d, :e]
              }
+    end
+  end
+
+  describe "build_case!/4" do
+    # TODO
+  end
+
+  describe "matches?" do
+    test "variable matches anything" do
+      assert Sum.matches?(quote(do: var), 1)
+      assert Sum.matches?(quote(do: var), 1.2)
+      assert Sum.matches?(quote(do: var), :ok)
+      assert Sum.matches?(quote(do: var), {})
+      assert Sum.matches?(quote(do: var), {1})
+      assert Sum.matches?(quote(do: var), {1, []})
+      assert Sum.matches?(quote(do: var), {1, [], %{}})
+      assert Sum.matches?(quote(do: var), [])
+      assert Sum.matches?(quote(do: var), [1])
+      assert Sum.matches?(quote(do: var), [1, []])
+      assert Sum.matches?(quote(do: var), [1, [], %{}])
+      assert Sum.matches?(quote(do: var), %{})
+      assert Sum.matches?(quote(do: var), %{name: "Sarah"})
+      assert Sum.matches?(quote(do: var), %__MODULE__{})
+      assert Sum.matches?(quote(do: var), %__MODULE__{field: 1})
+      assert Sum.matches?(quote(do: var), quote(do: whatever))
+    end
+
+    test "matching atoms" do
+      assert Sum.matches?(:ok, :ok)
+      assert Sum.matches?(:ok, quote(do: atom))
+      assert Sum.matches?(:ok, quote(do: atom()))
+    end
+
+    test "unmatching atom patterns" do
+      refute Sum.matches?(:ok, :nope)
+      refute Sum.matches?(:ok, 1)
+      refute Sum.matches?(:ok, 1.1)
+      refute Sum.matches?(:ok, 1.1)
+      refute Sum.matches?(:ok, [1])
+      refute Sum.matches?(:ok, {1})
+      refute Sum.matches?(:ok, {})
+      refute Sum.matches?(:ok, {1})
+      refute Sum.matches?(:ok, {1, []})
+      refute Sum.matches?(:ok, {1, [], %{}})
+      refute Sum.matches?(:ok, [])
+      refute Sum.matches?(:ok, [1])
+      refute Sum.matches?(:ok, [1, []])
+      refute Sum.matches?(:ok, [1, [], %{}])
+      refute Sum.matches?(:ok, %{})
+      refute Sum.matches?(:ok, %{name: "Sarah"})
+      refute Sum.matches?(:ok, %__MODULE__{})
+      refute Sum.matches?(:ok, %__MODULE__{field: 1})
+      refute Sum.matches?(:ok, quote(do: whatever))
     end
   end
 end
